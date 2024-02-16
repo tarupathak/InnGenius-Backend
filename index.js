9de5cfa87df21f03973e181bb6efd64d28bf717b
tarupathak <pathaktaru2002@gmail.com>
2024-02-16 18:19:23 +0530

    Setup env and connected to DB

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("COnnected to mongodb");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () =>{
+    console.log("mongoDB disconnected");
+})
+
+mongoose.connection.on("connected", () =>{
+    console.log("mongoDB connected");
+})
+
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend");
+});
