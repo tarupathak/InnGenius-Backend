568d1c031ffd3e4b13ba9976eac27ac86e06cef9
tarupathak <pathaktaru2002@gmail.com>
2024-02-20 21:00:29 +0530

    Added jwt auth

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,27 @@
 import express from "express";
+// import {
+//   deleteUser,
+//   getUsers,
+//   getUser,
+//   updateUser,
+// } from "../controllers/user";
 
 const router = express.Router();
 
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.send("Hello user, you are logged in");
+// });
 
+// //Update
+// router.put("/:id", updateUser);
+
+// //Delete
+// router.delete("/:id", deleteUser);
+
+// //Get
+// router.get("/:id", getUser);
+
+// //Get all
+// router.get("/", getUsers);
 
 export default router;
